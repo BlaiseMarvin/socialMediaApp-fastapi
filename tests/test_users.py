f137d6732bf2bbb9ab1cb9d308cf1cc79699c2d6
Blaise Marvin Rusoke <blaisemavin@gmail.com>
2022-04-20 16:39:33 +0300

    .

--- tests/test_users.py
+++ tests/test_users.py
@@ -13,23 +13,20 @@ from jose import JWTError,jwt
 
 def test_create_user(client):
     res=client.post("/users/",json={'email':'hellothere123@gmail.com','password':'password123'})
-    # new_user=schemas.UserOut(**res.json())
-    # assert new_user.email == 'hellothere123@gmail.com'
-    # assert res.status_code == 201
-    print(res.json())
+    new_user=schemas.UserOut(**res.json())
+    assert new_user.email == 'hellothere123@gmail.com'
+    assert res.status_code == 201
 
 def test_login_user(client,test_user):
     res=client.post("/login",data={'username':test_user['email'],'password':test_user['password']})
-    # login_res=schemas.Token(**res.json())
-    # # print(res.json())
-    # payload=jwt.decode(login_res.access_token,settings.secret_key,algorithms=[settings.algorithm])
+    login_res=schemas.Token(**res.json())
+    # print(res.json())
+    payload=jwt.decode(login_res.access_token,settings.secret_key,algorithms=[settings.algorithm])
 
-    # id=payload.get("user_id")
-    print(res.json())
+    id=payload.get("user_id")
 
-    # assert id == test_user['id']
-    # assert login_res.token_type == "bearer"
-    print(res.json())
+    assert id == test_user['id']
+    assert login_res.token_type == "bearer"
 
 @pytest.mark.parametrize("email,password,status_code",[
     ("wrongemail@gmail.com","password123",403),
@@ -40,8 +37,8 @@ def test_login_user(client,test_user):
 ])
 def test_incorrect_login(test_user,client,email,password,status_code):
     res=client.post("/login",data={'username':email,'password':password})
-    print(res.json())
-    # assert res.status_code == status_code
+
+    assert res.status_code == status_code
     #assert res.json().get('detail') == 'Invalid Credentials'
 
 
